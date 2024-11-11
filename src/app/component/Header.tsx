86c4d7a6b85c98298a71c3182c8e593e401701fc
Afnaniagq <165085097+Afnaniagq@users.noreply.github.com>
2024-11-11 19:47:52 +0500

    navbar

--- src/app/component/Header.tsx
+++ src/app/component/Header.tsx
@@ -1,33 +1,96 @@
+import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import React from "react";
+import { LuMenuSquare } from "react-icons/lu";
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@/components/ui/sheet"
 
 function Header(){
     return(
+      <header className="bg-black h-20">
+      <div  className="max-w-7xl  px-4 md:px-6 lg:px-8  ">
+          {/* <div   className="flex items-center justify-between h-16"> */}
+              {/* <div  className="flex items-center"> */}
+                 <div className="">
+                     <div className="flex ">
+                     <Image     src="/logo.png" alt="hijab" width={200} height={200}  className="size-[85px] fill-slate-200"></Image> 
+                     <h2 className="text-white text-[20px] mt-12 font-semibold mr-12 hover:translate-x-4 duration-[2s]">Hijab Corner</h2>
+                  
+<div className="flex items-end ml-auto mb-3">
+                   
+            <ul className="hidden md:block   ">
+            <li  className="space-x-6 text-[28px] text-white font-serif ">
+            <Link href="/" className="hover:text-slate-700 hover:bg-white rounded-md " >Home</Link>
+            <Link href="/about" className="hover:text-slate-700 hover:bg-white rounded-md ">About</Link>
+            <Link href="/contact" className="hover:text-slate-700 hover:bg-white rounded-md ">Contact</Link>
+            </li>
+            </ul>
+            </div>
+            <Sheet>
+      <SheetTrigger><LuMenuSquare  className=" md:hidden size-12 mx-2 fill-white" /></SheetTrigger>
+        <SheetContent >
+          <h2 className="pl-4 text-[25px] font-serif space-y-4 text-slate-700 font-bold ">List</h2>
+          <div className="bg-red-500 h-[2px] w-[390px] mb-3"></div>
+          <ul className="">
+            <li  className="space-y-6 pl-4 text-[25px]  text-slate-700 font-serif flex flex-col">
+            <Link href="/" className="hover:text-slate-700 " >Home</Link>
+            <Link href="/about" className="hover:text-slate-700 ">About</Link>
+            <Link href="/contact" className="hover:text-slate-700 ">Contact</Link>
+            </li>
+            </ul>
+              </SheetContent>
+      </Sheet>
+            </div>     
+
+      </div>
+      </div>
+            </header>
+
+    )
+}
+export default Header
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+// function Header(){
+//     return(
     
-        <header className="bg-black p-3">
-         <div  className="max-w-7xl mx- px-4 sm:px-6 lg:px-8">
-             <div   className="flex items-center justify-between h-16">
-                 <div  className="flex items-center">
-                    <div className="flex-shrink-0">
-                        <a href="" className="flex flex-row">
-                        <Image     src="/logo.png" alt="hijab" width={200} height={200}  className="size-[100px]  fill-slate-200"></Image> 
-                        <h2 className="text-white text-[20px] mt-12 font-semibold mr-12 hover:translate-x-4 duration-[2s]">Hijab Corner</h2>
-                        </a>
-          </div>
-            </div>
-                    <div  className="ml-4 flex  justify-items-center j justify-self-center  space-x-4">
-                     <Link href="/" className="text-white round-lg p-2 text-[20px] f hover:bg-white rounded-lg font-semibold hover:text-black ">Home</Link>
-                     <Link href="/about" className="text-white round-lg p-2 text-[20px] font-semibold hover:bg-white rounded-lg hover:text-black">About</Link>
-                     <Link href="/contact" className="text-white round-lg p-2 text-[20px] font-semibold hover:bg-white rounded-lg hover:text-black">Contact</Link>
-            </div>
+//         <header className="bg-black p-3">
+//          <div  className="max-w-7xl mx- px-4 sm:px-6 lg:px-8">
+//              <div   className="flex items-center justify-between h-16">
+//                  <div  className="flex items-center">
+//                     <div className="flex-shrink-0">
+//                         <a href="" className="flex flex-row">
+//                         <Image     src="/logo.png" alt="hijab" width={200} height={200}  className="size-[100px]  fill-slate-200"></Image> 
+//                         <h2 className="text-white text-[20px] mt-12 font-semibold mr-12 hover:translate-x-4 duration-[2s]">Hijab Corner</h2>
+//                         </a>
+//           </div>
+//             </div>
+// //                     <div  className="ml-4 flex  justify-items-center j justify-self-center  space-x-4">
+// //                      <Link href="/" className="text-white round-lg p-2 text-[20px] f hover:bg-white rounded-lg font-semibold hover:text-black ">Home</Link>
+// //                      <Link href="/about" className="text-white round-lg p-2 text-[20px] font-semibold hover:bg-white rounded-lg hover:text-black">About</Link>
+// //                      <Link href="/contact" className="text-white round-lg p-2 text-[20px] font-semibold hover:bg-white rounded-lg hover:text-black">Contact</Link>
+// //             </div>
 
             
           
-          </div>
-          </div>
-        </header>
-     );
-};
-export default Header
+// //           </div>
+// //           </div>
+// //         </header>
+// //      );
+// };
+// export default Header
        
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Image from "next/image";
         <section>
       <div  className="basis-[45%] md:order-2 order-1 p-[5px] group">
      
-  <Image     src="/top.webp" alt="hijab" width={1500} height={1200} className="hover:scale-95  border-black duration-[3s] hover:translate-y-[10px] " ></Image> 
+  <Image     src="/top.webp" alt="hijab" width={1500} height={1200} className="  border-black  " ></Image> 
             
           </div>
          </section>
@@ -25,44 +25,44 @@ import Image from "next/image";
             </div>
           </div>
           <div  className="basis-[45%]  rounded-lg md:order-2 order-1 p-[20px] group">
-            <Image className="hover:translate-y-7 duration-[2s] rounded-xl border-black"   src="/scarf.jpg" alt="hijab" width={550} height={500}></Image>
+            <Image className="hover:translate-y-4 duration-[2s] rounded-xl border-black"   src="/scarf.jpg" alt="hijab" width={550} height={500}></Image>
             
           </div>
         </div>
       </div>
 
-      <div className="max-w-[1320px] mx-auto">
+      <div className="max-w-[132] mx-auto">
           <h2 className="text-center text-bold font-bold hover:text-[#C3A275] text-[30px] p-[15px] "> ABAYA&apos;S _ HIJAB</h2>
-        <div className="flex flex-wrap ">
+        <div className="flex flex-wrap  ">
           <div className="basis-[30%] m-[1.5%] shadow-lg  py-[20px] text-center group hover:translate-y-[17px]">
           <Image src="/abayo.webp" alt="pic" width={370} height={370}
             className="w-[45] h-[40] mx-auto group-hover:scale-[1.1]"></Image>
-            <h2 className="text-[22px] font-bold py-[15px] group-hover:pt-9  hover:text-[#C3A275] ">ABYAS</h2>
+            <h2 className="text-[16px] md:text-[22px] font-bold py-[15px] group-hover:pt-9  hover:text-[#C3A275] ">ABYAS</h2>
           </div>
           <div className="basis-[30%] m-[1.5%] shadow-lg py-[20px] text-center group hover:translate-y-[17px] ">
             <Image src="/kid.webp" alt="pic" width={370} height={370}
             className=" w-[45] h-[40] mx-auto group-hover:scale-[1.1]"/>
-            <h2 className="text-[22px] font-bold py-[15px]  group-hover:pt-9  hover:text-[#C3A275] ">KID&apos;S ABAYAS</h2>
+            <h2 className="text-[16px] md:text-[22px] font-bold py-[15px]  group-hover:pt-9  hover:text-[#C3A275] ">KID&apos;S ABAYAS</h2>
           </div>
           <div className="basis-[30%] m-[1.5%] shadow-lg py-[20px] text-center  group hover:translate-y-[17px]  ">
             <Image src="/purple.webp" alt="pic" width={350} height={350}
             className="w-[45] h-[40] mx-auto  group-hover:scale-[1.1]"/>
-            <h2 className="text-[22px] font-bold py-[15px]  group-hover:pt-9  hover:text-[#C3A275] ">PARTY WEAR</h2>
+            <h2 className="text-[16px] md:text-[22px]  font-bold py-[15px]  group-hover:pt-9  hover:text-[#C3A275] ">PARTY WEAR</h2>
           </div>
           <div className="basis-[30%] m-[1.5%] shadow-lg py-[20px] text-center  group hover:translate-y-[17px] ">
             <Image src="/maxi.webp" alt="pic" width={370} height={370}
             className="w-[45] h-[40] mx-auto  group-hover:scale-[1.1]"/>
-            <h2 className="text-[22px] font-bold py-[15px] group-hover:pt-9  hover:text-[#C3A275] ">MAXI ABAYA</h2>
+            <h2 className="text-[16px] md:text-[22px]  font-bold py-[15px] group-hover:pt-9  hover:text-[#C3A275] ">MAXI ABAYA</h2>
           </div>
           <div className="basis-[30%] m-[1.5%] shadow-lg py-[20px] text-center group hover:translate-y-[17px]">
             <Image src="/Oyoon.webp" alt="pic" width={350} height={350}
             className="w-[45] h-[40] mx-auto group-hover:scale-[1.1]"/>
-            <h2 className="text-[22px] font-bold py-[15px] group-hover:pt-9  hover:text-[#C3A275] ">FANCY WEAR</h2>
+            <h2 className="text-[16px] md:text-[22px]  font-bold py-[15px] group-hover:pt-9  hover:text-[#C3A275] ">FANCY WEAR</h2>
           </div>
           <div className="basis-[30%] m-[1.5%] shadow-lg py-[20px]   text-center group hover:translate-y-[17px] ">
             <Image src="/brownhijab.webp" alt="pic" width={370} height={370}
             className="w-[45] h-[40] mx-auto group-hover:scale-[1.1]"/>
-            <h2 className="text-[22px] font-bold py-[15px]  group-hover:pt-9  hover:text-[#C3A275] ">HIJAB WEAR</h2>
+            <h2 className="text-[16px] md:text-[22px]  font-bold py-[15px]  group-hover:pt-9  hover:text-[#C3A275] ">HIJAB WEAR</h2>
           </div>
         </div>
       </div>
@@ -74,32 +74,32 @@ import Image from "next/image";
           <div className="basis-[30%] m-[1.5%] shadow-lg  py-[20px] text-center group hover:translate-y-[17px] duration-[2s] ">
             <Image src="/bluepin.webp" alt="pic" width={400} height={400}
             className="w-[45] h-[40] mx-auto group-hover:scale-100 "/>
-            <h2 className="text-[22px] font-bold py-[15px]   hover:text-[#C3A275]">TUSPINS</h2>
+            <h2 className="text-[16px] md:text-[22px]  font-bold py-[12px]   hover:text-[#C3A275]">TUSPINS</h2>
           </div>
           <div className="basis-[30%] m-[1.5%] shadow-lg py-[20px] text-center group hover:translate-y-[17px]  duration-[2s]">
             <Image src="/bruch.jpg" alt="pic" width={400} height={400}
             className=" w-[45] h-[40] mx-auto group-hover:scale-100"/>
-            <h2 className="text-[22px] font-bold py-[15px]    hover:text-[#C3A275]">SPHERICAL-COIL PINS</h2>
+            <h2 className="text-[16px] md:text-[22px]  font-bold py-[15px]    hover:text-[#C3A275]">SPHERICAL PINS</h2>
           </div>
           <div className="basis-[30%] m-[1.5%] shadow-lg py-[20px] text-center  group hover:translate-y-[17px]  duration-[2s] ">
             <Image src="/pinkpin.webp" alt="pic" width={400} height={400}
             className="w-[45] h-[40] mx-auto  group-hover:scale-100"/>
-            <h2 className="text-[22px] font-bold py-[15px]    hover:text-[#C3A275]">BALL TUSPINS</h2>
+            <h2 className="text-[16px] md:text-[22px]  font-bold py-[15px]    hover:text-[#C3A275]">BALL TUSPINS</h2>
           </div>
           <div className="basis-[30%] m-[1.5%] shadow-lg py-[20px] text-center  group hover:translate-y-[17px]  duration-[2s]">
             <Image src="/silver.webp" alt="pic" width={400} height={400}
             className="w-[45] h-[40] mx-auto  group-hover:scale-100"/>
-            <h2 className="text-[22px] font-bold py-[15px]   hover:text-[#C3A275]">SILVER HIJAB PIN</h2>
+            <h2 className="text-[16px] md:text-[22px]  font-bold py-[15px]   hover:text-[#C3A275]">SILVER HIJAB PIN</h2>
           </div>
           <div className="basis-[30%] m-[1.5%] shadow-lg py-[20px] text-center group hover:translate-y-[17px] duration-[2s]">
             <Image src="/moonlight.webp" alt="pic" width={400} height={400}
             className="w-[45] h-[40] mx-auto group-hover:scale-100"/>
-            <h2 className="text-[22px] font-bold py-[15px]   hover:text-[#C3A275]">MOONLIGHT PIN</h2>
+            <h2 className="text-[16px] md:text-[22px]  font-bold py-[15px]   hover:text-[#C3A275]">MOONLIGHT PIN</h2>
           </div>
           <div className="basis-[30%] m-[1.5%] shadow-lg py-[20px]   text-center group hover:translate-y-[17px]  duration-[2s]">
             <Image src="/blue-magnet.webp" alt="pic" width={400} height={400}
             className="w-[45] h-[40] mx-auto group-hover:scale-100"/>
-            <h2 className="text-[22px] font-bold py-[15px]    hover:text-[#C3A275]">BLUE MAGNET PIN</h2>
+            <h2 className="text-[16px] md:text-[22px]  font-bold py-[15px]    hover:text-[#C3A275]">MAGNET PIN</h2>
           </div>
         </div>
       </div>
@@ -121,33 +121,33 @@ import Image from "next/image";
           <div className="basis-[30%] m-[1.5%] shadow-lg  py-[20px] text-center group hover:translate-y-[17px]  duration-[2s]">
             <Image src="/cap.webp" alt="pic"  width={280} height={280}
             className="w-[45] h-[40] mx-auto group-hover:scale-100"/>
-            <h2 className="text-[22px] font-bold py-[15px]  hover:text-[#C3A275] ">UNDER SCARF</h2>
+            <h2 className="text-[16px] md:text-[22px]  font-bold py-[15px]  hover:text-[#C3A275] ">UNDER SCARF</h2>
           </div>
           <div className="basis-[30%] m-[1.5%] shadow-lg py-[20px] text-center group hover:translate-y-[17px]  duration-[2s]">
             <Image src="/Black.webp" alt="pic"  width={280} height={280}
             className=" w-[45] h-[40] mx-auto group-hover:scale-100"/>
-            <h2 className="text-[22px] font-bold py-[15px]   hover:text-[#C3A275] ">ARM SLEEVES</h2>
+            <h2 className="text-[16px] md:text-[22px]  font-bold py-[15px]   hover:text-[#C3A275] ">ARM SLEEVES</h2>
           </div>
           <div className="basis-[30%] m-[1.5%] shadow-lg py-[20px] text-center  group hover:translate-y-[17px]  duration-[2s]">
             <Image src="/scrunchies.webp"  alt="pic"  width={280} height={280}
             className="w-[45] h-[40] mx-auto  group-hover:scale-100"/>
-            <h2 className="text-[22px] font-bold py-[15px]   hover:text-[#C3A275] ">SCRUNCHIES</h2>
+            <h2 className="text-[16px] md:text-[22px]  font-bold py-[15px]   hover:text-[#C3A275] ">SCRUNCHIES</h2>
           </div>
 
           <div className="basis-[30%] m-[1.5%] shadow-lg  py-[20px] text-center group hover:translate-y-[17px]  duration-[2s]">
             <Image src="/tasbeeh.webp" alt="pic"  width={300} height={300}
             className="w-[45] h-[40] mx-auto group-hover:scale-100"/>
-            <h2 className="text-[22px] font-bold py-[15px]  hover:text-[#C3A275] ">WHITE PEARL</h2>
+            <h2 className="text-[16px] md:text-[22px]  font-bold py-[15px]  hover:text-[#C3A275] ">WHITE PEARL</h2>
           </div>
           <div className="basis-[30%] m-[1.5%] shadow-lg py-[20px] text-center group hover:translate-y-[17px]  duration-[2s]">
             <Image src="/niqab.webp" alt="pic"  width={310} height={310}
             className=" w-[45] h-[40] mx-auto group-hover:scale-100"/>
-            <h2 className="text-[22px] font-bold py-[15px]   hover:text-[#C3A275] ">NIQAB</h2>
+            <h2 className="text-[16px] md:text-[22px]  font-bold py-[15px]   hover:text-[#C3A275] ">NIQAB</h2>
           </div>
           <div className="basis-[30%] m-[1.5%] shadow-lg py-[20px] text-center  group hover:translate-y-[17px]  duration-[2s]">
             <Image src="/luxehijab.webp" alt="pic"  width={350} height={350}
             className="w-[45] h-[40] mx-auto  group-hover:scale-100"/>
-            <h2 className="text-[22px] font-bold py-[15px]   hover:text-[#C3A275] ">LUXE HIJAB</h2>
+            <h2 className="text-[16px] md:text-[22px]  font-bold py-[15px]   hover:text-[#C3A275] ">LUXE HIJAB</h2>
           </div>
          
         </div>
@@ -157,12 +157,12 @@ import Image from "next/image";
           </section>
       </section>
     </section>
-     <section className="bg-black py-3 size-auto grid lg:grid-row-auto flex-wrap">
+     <section className="bg-black  size-auto grid lg:grid-row-auto flex-wrap">
            <h1 className="text-[20px] md:text-[30px] lg:text-[40px] font-bold text-center py-[40px] text-white">Information</h1>
            <div className="max-w-[1170px] mx-auto grid grid-cols-1 md:grid-cols-[40%_auto] gap-[30px] flex-wrap">
              <div className="flex gap-15px mb-12  text-white" >
-              <ul>
-            <li className="text-[20px] font-bold font-mono ">FOLLOW US</li>
+              <ul className="ml-16  md:ml-8">
+            <li className="text-[20px]  font-bold font-mono  hover:text-blue-600">FOLLOW US</li>
             <li  >Who we are</li>
             <li  >Return & Exchange Policy</li>
             <li  >Order Placement & Confirmation</li>
@@ -172,10 +172,10 @@ import Image from "next/image";
             <li>FAQs</li> 
             </ul>
              </div>
-             <div  className="basis-[45%] md:order-2 order-1 p-[20px] group">
-             <h2 className=" sm:text-[10px] lg:text-[20px]  font-bold py-[15px] px-[30px] pl-20 text-end text-white hover:text-bold">Subscribe now to be the first to know about our promotional offers!</h2>
+             <div  className="basis-[45%] md:order-2 order-1 p-[20px] ">
+             <h2 className=" text-15px] md:text-[20px]  font-bold py-[15px] px-[30px] pl-20 text-end text-white hover:text-bold">Subscribe now to be the first to know about our promotional offers!</h2>
              <div className="pl-80 pt-14">
-              <p className="text-white font-mono text-center">Copyright © 2024, HijabCorner </p>
+              <p className=" text-[15px] md:text-[20px] text-white hover:text-blue-600 font-mono text- pr-10 ">Copyright © 2024, HijabCorner </p>
              </div>
            </div>
            </div>
